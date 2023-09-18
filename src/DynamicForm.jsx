a5a46c01b13d70948ac28122d07ffa7f6df7dd59
Ryan Wilkins <wilkinsrs@gmail.com>
2023-09-18 05:19:24 -0400

    form successfully works with local storage

--- src/DynamicForm.jsx
+++ src/DynamicForm.jsx
@@ -2,15 +2,9 @@ import { React, useState } from "react"
 
 export default function DynamicForm({defaultText}) {
     const [edit, setEdit] = useState(false)
-    const [webKey, setWebKey] = useState("initial web key")
-    const [sdkEndpoint, setSDKEndpoint] = useState("initial sdk Endpoint")
-    const [apiKey, setAPIKey] = useState("initial api key")
-
-    // the initial state values for webkey, sdk, api need to pull from local storage
-    // the submitData function needs to be tested to actually set the values
-    // the cancelChange function needs to return the state values to local storage values
-    // the App connection state needs to be set up in App(), and then passed to DynamicForm as props
-    // those Props need to be tested
+    const [webKey, setWebKey] = useState(JSON.parse(localStorage.getItem("stitchboxBrazeConnection")).webApp)
+    const [sdkEndpoint, setSDKEndpoint] = useState(JSON.parse(localStorage.getItem("stitchboxBrazeConnection")).sdk)
+    const [apiKey, setAPIKey] = useState(JSON.parse(localStorage.getItem("stitchboxBrazeConnection")).api)
     
     // IT IS WORKING!!!
     function submitData() {
@@ -19,28 +13,29 @@ export default function DynamicForm({defaultText}) {
             sdk: sdkEndpoint,
             api: apiKey
         }))
+        setEdit(!edit)
     }
 
     function cancelChange() {
-        setWebKey("initial web key")
-        setSDKEndpoint("initial sdk Endpoint")
-        setAPIKey("initial api key")
+        setWebKey(JSON.parse(localStorage.getItem("stitchboxBrazeConnection")).webApp)
+        setSDKEndpoint(JSON.parse(localStorage.getItem("stitchboxBrazeConnection")).sdk)
+        setAPIKey(JSON.parse(localStorage.getItem("stitchboxBrazeConnection")).api)
         setEdit(!edit)
     }
     
     return (
         <>
         <label>Web App API Key</label>
-        {edit && ( <input id="webApp" value={webKey} onChange={(e) => setWebKey(e.target.value)}></input>)}
+        {edit && ( <input id="webApp" value={webKey} onChange={(e) => setWebKey(e.target.value)} required></input>)}
         {!edit && ( <input disabled placeholder={webKey}></input>)}
         <label>SDK Endpoint</label>
-        {edit && ( <input id="sdk" value={sdkEndpoint} onChange={(e) => setSDKEndpoint(e.target.value)}></input>)}
+        {edit && ( <input id="sdk" value={sdkEndpoint} onChange={(e) => setSDKEndpoint(e.target.value)} required></input>)}
         {!edit && ( <input disabled placeholder={sdkEndpoint}></input>)}
         <label>API Key</label>
-        {edit && ( <input id="api" value={apiKey} onChange={(e) => setAPIKey(e.target.value)}></input>)}
+        {edit && ( <input id="api" value={apiKey} onChange={(e) => setAPIKey(e.target.value)} required></input>)}
         {!edit && ( <input disabled placeholder={apiKey}></input>)}
         {!edit && ( <button onClick={() => setEdit(!edit)}>Change Connection</button>)}
-        {edit && ( <button>Save Connection</button>)}
+        {edit && ( <button onClick={submitData}>Save Connection</button>)}
         {edit && ( <button onClick={cancelChange}>Cancel</button>)}
         </>    
     )
